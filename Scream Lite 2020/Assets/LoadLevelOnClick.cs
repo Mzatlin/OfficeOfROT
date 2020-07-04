23665420dac1a83645a2c47edd0b052089ed8b01
Mzatlin <michael.zatlin@yahoo.com>
2020-07-04 13:03:14 -0700

    Added basic title screen that handles playing and leaving the game.

---
+++ Scream Lite 2020/Assets/LoadLevelOnClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelOnClick : MonoBehaviour
+{
+    public string sceneName;
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
