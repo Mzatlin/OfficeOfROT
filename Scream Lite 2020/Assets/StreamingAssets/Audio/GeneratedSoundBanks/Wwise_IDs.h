5e3a5c5f141be218ebfd34835c816ad7498e0499
Zack-Booth <garzazack@gmail.com>
2020-07-03 23:12:27 -0400

    object and character sfx in wwise
    implemented vending machine sfx in unity, trouble with watercooler

--- Scream Lite 2020/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Scream Lite 2020/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,9 +13,19 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID BILLY = 818946243U;
         static const AkUniqueID DOOROPEN = 1404805401U;
         static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID FRIDGE = 2337612214U;
+        static const AkUniqueID LIGHTER = 282445894U;
+        static const AkUniqueID MARSHA = 1096578879U;
         static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID PRINTER = 1425580747U;
+        static const AkUniqueID SOLLY = 694286824U;
+        static const AkUniqueID VENDING_MACHINE = 3790536434U;
+        static const AkUniqueID WATER_COOLER = 2035707747U;
+        static const AkUniqueID WILLY = 809636474U;
     } // namespace EVENTS
 
     namespace BANKS
