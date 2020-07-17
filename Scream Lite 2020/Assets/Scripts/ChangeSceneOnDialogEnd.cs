f21dd2007b4acf9b263d49f397eace16e6e01265
Mzatlin <michael.zatlin@yahoo.com>
2020-07-16 21:47:01 -0700

    Player now returns to the title screen after finishing the final dialogue.

---
+++ Scream Lite 2020/Assets/Scripts/ChangeSceneOnDialogEnd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneOnDialogEnd : MonoBehaviour
+{
+    public string name;
+    public DialogueLoader load; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        load.OnEnd += HandleEnd;
+    }
+
+    private void HandleEnd()
+    {
+        SceneManager.LoadScene(name, LoadSceneMode.Single);
+    }
+
+}
