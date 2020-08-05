7add8933df4f05fde70042f78034a8c672ee39f3
Mzatlin <michael.zatlin@yahoo.com>
2020-08-04 22:17:25 -0700

    Player can no longer interact with objects during final chase scene. Cleaned up npc message class.

--- Scream Lite 2020/Assets/Scripts/WriteNPCMessageOnInteraction.cs
+++ Scream Lite 2020/Assets/Scripts/WriteNPCMessageOnInteraction.cs
@@ -5,27 +5,19 @@ using UnityEngine;
 public class WriteNPCMessageOnInteraction : HandleInteractBase, INPCWrite
 {
     ILoadDialogue loader;
-    bool isLoaded = false;
     public DialogueLoader dialogueWrite;
-    ICameraRaycast raycast;
-    Camera cam;
     public DialogSO dialogueSO;
     public DialogSO dialogToLoad { get => dialogueSO; set => dialogueSO = value; }
-
-    public bool IsLoaded { get => isLoaded; set => isLoaded = value; }
 
     protected override void Awake()
     {
         base.Awake();
         dialogueWrite.OnEnd += HandleEnd;
-        cam = Camera.main;
-        raycast = cam.GetComponent<ICameraRaycast>();
         loader = GetComponent<ILoadDialogue>();
     }
     protected override void HandleEnd()
     {
         base.HandleEnd();
-        isLoaded = false;
         if (IsInteracting)
         {
             dialogueWrite.OnEnd -= HandleEnd;
@@ -37,29 +29,5 @@ public class WriteNPCMessageOnInteraction : HandleInteractBase, INPCWrite
         base.HandleInteraction();
         loader.SetDialogue(dialogueSO);
     }
-
- /*   void Update()
-    {
-        if (IsInteracting)
-        {
-            raycast.CanCast = false;
-            if (Vector2.Distance(transform.position, player.transform.position) < 2.5)
-            {
-                LoadDialogue();
-            }
-
-        }
-    }
-
-   public void LoadDialogue()
-    {
-        if (!isLoaded)
-        {
-            dialogueWrite.SetupDialougeWriter(dialogueSO);
-            isLoaded = true;
-        }
-
-    }
-    */
 
 }
