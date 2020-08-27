4547636b249c93c29cecaa85d3e736c53dc60148
Mzatlin <michael.zatlin@yahoo.com>
2020-08-26 21:26:46 -0700

    Door sound now plays when the player exits the first two levels.

---
+++ Scream Lite 2020/Assets/Scripts/PlaySoundOnLoad.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnLoad : MonoBehaviour
+{
+    [SerializeField]
+    private AK.Wwise.Event myEvent = null;
+    ILoadScene scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scene = GetComponent<ILoadScene>();
+        if (scene != null)
+        {
+            scene.OnLevelLoad += HandleLoad;
+        }
+    }
+
+    void OnDestroy()
+    {
+        scene.OnLevelLoad -= HandleLoad;
+        myEvent.Stop(gameObject);
+    }
+
+    private void HandleLoad()
+    {
+            myEvent.Post(gameObject);
+    }
+
+}
